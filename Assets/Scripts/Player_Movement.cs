049d76d6b24c7bfdd50884e3cf7c5a07e749961d
Ignacio Tapia <114392929+IgTapia02@users.noreply.github.com>
2024-07-16 14:43:37 +0200

    Movimiento player

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rb;
+    Vector2 movement;
+    [SerializeField]
+    float moveSpeed;
+
+    [Header("Dash")]
+    [SerializeField]
+    float dashSpeed;
+    [SerializeField]
+    float dashDuration;
+    [SerializeField]
+    float dashCooldown;
+    bool isDashing;
+    bool canDash;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        canDash = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        movement = new Vector2(moveX, moveY).normalized;
+
+        if (Input.GetAxisRaw("Jump")>0 && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+        Debug.Log(movement);
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDashing)
+            return;
+
+        rb.velocity = new Vector2(movement.x * moveSpeed, movement.y * moveSpeed);
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        if(Input.GetAxisRaw("Vertical")==0 && Input.GetAxisRaw("Horizontal") == 0)
+        {
+            rb.velocity = new Vector2(0.71f * dashSpeed, 0.71f * dashSpeed);
+        }
+        else
+        {
+            rb.velocity = new Vector2(movement.x * dashSpeed, movement.y * dashSpeed);
+        }
+
+        yield return new WaitForSeconds(dashDuration);
+        isDashing = false;
+
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 }
