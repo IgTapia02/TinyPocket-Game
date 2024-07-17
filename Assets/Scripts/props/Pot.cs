ccfab44d1aeaf2c0da6e8b1955ba15eb949c20ff
MikelJulian <mikeljulian07@gmail.com>
2024-07-17 12:12:07 +0200

    UI

---
+++ Assets/Scripts/props/Pot.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pot : MonoBehaviour
+{
+   
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("pj");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y<player.transform.position.y) {
+
+        }
+    }
+    private void ChangeLayer()
+    {
+        this.gameObject.GetComponent()
+    }
+}
