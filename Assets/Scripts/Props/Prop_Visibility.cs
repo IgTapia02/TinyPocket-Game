b399241371ab5bc04461aa45cafac78a039b38fa
Ignacio Tapia <114392929+IgTapia02@users.noreply.github.com>
2024-07-18 11:46:20 +0200

    Visibility props

--- Assets/Scripts/Props/Prop_Visibility.cs
+++ Assets/Scripts/Props/Prop_Visibility.cs
@@ -4,23 +4,34 @@ using UnityEngine;
 
 public class Prop_Visibility : MonoBehaviour
 {
-   
+
     GameObject player;
+    SpriteRenderer sr;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("pj");
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.y<player.transform.position.y) {
-
+        if (transform.position.y <= player.transform.position.y && sr.sortingOrder == 0)
+        {
+            ChangeLayer();
+        }
+        if (transform.position.y > player.transform.position.y && sr.sortingOrder == 3)
+        {
+            ChangeLayer();
         }
     }
     private void ChangeLayer()
     {
-        this.gameObject.GetComponent();
+        Debug.Log("hola");
+        if (sr.sortingOrder == 0)
+            sr.sortingOrder = 3;
+        else if (sr.sortingOrder == 3)
+            sr.sortingOrder = 0;
     }
 }
