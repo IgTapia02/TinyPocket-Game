2f8dcc8b29dda429e9144ab57dbd6589b4bbfba9
MikelJulian <mikeljulian07@gmail.com>
2024-07-22 16:46:23 +0200

    creditos

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -7,14 +7,25 @@ using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
     public TMP_Text startButtonText;
+    public TMP_Text creditsButtonText;
+
     public TMP_Text quitButtonText;
 
     public void StartGame()
     {
         SceneManager.LoadScene("Mazmorra1");  
     }
+    public void Credits()
+    {
+        SceneManager.LoadScene("Creditos");
 
-    
+    }
+    public void GoBack()
+    {
+        SceneManager.LoadScene("Menu");
+
+    }
+
     public void QuitGame()
     {
         
